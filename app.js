28bb33bf91c6d5c6c62b0c85d608eaa6e234af4d
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-15 14:27:16 -0700

    [arrays-number] add extra array

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ console.log(favouriteNumber);
 var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers' ]
 /*arrays*/
 console.log(teams);
+
+//while loops
+
+var number = 10;
+var otherNumber = 1;
+while ( otherNumber < number ) {
+    console.log(otherNumber);
+    otherNumber += 1; //otherNumber = otherNumber + 1 and stop at 10
+}
+
+var numberTwo = 20;
+var myotherNumber = 10;
+
+while (myotherNumber < numberTwo){
+    console.log(myotherNumber); // print 10, 11, 12
+    myotherNumber += 1; // print 11, 12, 13
+}
+
+var numberThree = 30;
+var myotherNumberThree = 20;
+
+while (myotherNumberThree < numberThree){
+    console.log(myotherNumberThree);
+    myotherNumberThree += 1;
+}
