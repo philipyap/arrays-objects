2c64b1407482ec51effe699a512dc9d0cd106fa1
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-15 14:03:47 -0700

    [arrays] add arrays

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = 'Philip';/*string*/
+console.log(name);
+
+var myLocation = 'San Francisco Bay Area';
+console.log(myLocation);
+
+var favouriteNumber = 38; /*number*/
+console.log(favouriteNumber);
+
+var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers' ]
+/*arrays*/
+console.log(teams);
